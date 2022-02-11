72ae56960ec7f4b2e2133caf799bf50116718fe0
hshih <hshih@node1.thrift-testing.uwmadison744-f21-pg0.wisc.cloudlab.us>
2022-02-10 22:19:19 -0600

    Seveal machine in client.cpp

--- roundTrip/client.cpp
+++ roundTrip/client.cpp
@@ -15,14 +15,15 @@ using namespace ::apache::thrift::transport;
 int main(int argc, char **argv) {
   int port = 9090;
   ::std::shared_ptr<TSocket> socket(new TSocket("localhost", port));
-  //::std::shared_ptr<TSocket> socket(new TSocket("hshih@c220g2-011121.wisc.cloudlab.us", 22));
+  //::std::shared_ptr<TSocket> socket(new TSocket("10.10.1.1", port));
+ 
   ::std::shared_ptr<TTransport> transport(new TBufferedTransport(socket));
   ::std::shared_ptr<TProtocol> protocol(new TBinaryProtocol(transport));
   std::string message = "Hello Server! sent from client";
   struct timespec start, end;
   ServClient client(protocol);
   transport->open();
-  std::cout << "One Machine\n";
+  
   for (int i = 0; i < 20; ++i) {
   	clock_gettime(CLOCK_REALTIME, &start);
  	client.send(message);  
