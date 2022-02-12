b43d59b1f45653f74574170ba7075d4aa7b1478c
hshih25 <hshih25@wisc.edu>
2022-02-12 16:46:30 -0600

    Round trip test

--- roundTrip/gen-cpp/Serv.cpp
+++ roundTrip/gen-cpp/Serv.cpp
@@ -234,9 +234,9 @@ void ServClient::send(std::vector<int64_t> & _return, const std::string& str1)
   clock_gettime(CLOCK_REALTIME, &start);
   recv_send(_return);
   clock_gettime(CLOCK_REALTIME, &end);
-  std:: cout << _return[0] << " " << start.tv_sec << " " << _return[1] << " " << start.tv_nsec << "\n";
-  //std:: cout << (_return[0] - start.tv_sec) * 1000000000L + _return[1] - start.tv_nsec << "     ";
-  //std:: cout << (end.tv_sec - _return[0]) * 1000000000L + end.tv_nsec - _return[1] << "\n";
+  //std:: cout << _return[0] << " " << start.tv_sec << " " << _return[1] << " " << start.tv_nsec << "\n";
+  std:: cout << (_return[0] - start.tv_sec) * 1000000000L + _return[1] - start.tv_nsec << "     ";
+  std:: cout << (end.tv_sec - _return[0]) * 1000000000L + end.tv_nsec - _return[1] << "\n";
 }
 
 void ServClient::send_send(const std::string& str1)
